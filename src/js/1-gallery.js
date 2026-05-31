267f15195276867b6ee904a9c37edd85f6d60721
nick3fc <nick3fc@gmail.com>
2026-05-31 15:48:34 +0300

    5

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -96,25 +96,3 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
   showCounter: false,
 });
-// const galleryClick = document.querySelector('.gallery');
-
-// galleryClick.addEventListener('click', event => {
-//   event.preventDefault();
-//   if (event.target.nodeName !== 'IMG') {
-//     return;
-//   }
-//   const onClickGetUrl = event.target.dataset.source;
-//   console.log(onClickGetUrl);
-
-//   const instance = basicLightbox.create(`
-
-//     <img
-//       class="gallery-image-preview"
-//       src="${onClickGetUrl}"
-//       alt="${event.target.alt}"
-
-//     />
-
-// `);
-//   instance.show();
-// });
